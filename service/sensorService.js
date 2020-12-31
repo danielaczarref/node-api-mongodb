28f06bbdf4bf8b7a730dfffab9ed44aeb3983169
Daniela Carvalho Ferraz Nolasco Neves <daniela.carvalho@aluno.ecp.ufma.br>
2020-12-31 00:51:40 -0300

    R -file- removendo yarn lock

--- service/sensorService.js
+++ service/sensorService.js
@@ -8,6 +8,7 @@ function inserirNovoItem(body, res) {
      var db = require('../config/database');
         var item = db.Mongoose.model('temperatura', db.ItemSchema, 'temperatura');
         const novoUuid = uuid();
+        console.log('tipo: ', typeof body.latitude)
         var novoItem = new item({ uuid: novoUuid, owner: body.owner, measure: body.measure,
             latitude: body.latitude, longitude: body.longitude, topic: `sensor/${novoUuid}/data`});
         novoItem.save(function (err) {
